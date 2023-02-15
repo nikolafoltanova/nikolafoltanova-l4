fe9241845796cbc5cf5696acee9f179ea78e0edc
Nikola Foltánová <foltanova@ders.cz>
2023-02-15 23:41:12 +0100

    ukol4

--- main.js
+++ main.js
@@ -1,64 +1,57 @@
-// tady je místo pro náš program ->  https://kodim.cz/kurzy/javascript-vyuka/js-1/vstup-vystup-objekty/cv-objekty
-// Realitka
-const apartment = {
-	type: 'rent',
-	disposition: '3+1',
-	area: {
-		floorage: 100,
-		balcony: 2,
-		units: 'sqm',
+// tady je místo pro náš program - https://kodim.cz/kurzy/javascript-vyuka/js-1/retezce-podminky/cv-retezce
+// ukol 1
+const title = 'Harry Potter'
+console.log("Počet znaků: " + title.length)
+console.log("Velký název: " + title.toUpperCase())
+console.log("Prvních 5 písmen: " + title.slice(0, 5))
+console.log("Posledních 5 písmen: " + title.slice(title.length - 5, title.length))
+
+// ukol 2
+const email = prompt("Uveď svůj email")
+document.body.innerHTML += email
+let atIndex = email.indexOf('@')
+console.log(email.slice(0, atIndex))
+console.log(email.slice(atIndex + 1, email.length))
+const parsedEmail = {
+	userName: email.slice(0, atIndex),
+	domain: email.slice(atIndex + 1, email.length)
+}
+document.body.innerHTML += '<p>Uživatel: ' + parsedEmail.userName + '</p>'
+document.body.innerHTML += '<p>Doména: ' + parsedEmail.domain + '</p>'
+
+// ukol 3
+const street = prompt('Zadej ulici:')
+const houseNumber = prompt('Zadej číslo domu:')
+const city = prompt('Zadej město:')
+const zipCode = prompt('Zadej psč:')
+document.body.innerHTML +=
+'<address>' +
+	'<p>'+ street +' '+ houseNumber + '</p>'+
+	'<p>'+ zipCode + ' '+ city + '</p>'+
+'</address>'
+
+// knihovna
+const book1 = {
+    name: 'Harry Potter',
+    author: {
+		firstname: 'Joanne',
+		surname: 'Rowlingová'
 	},
-	city: 'Prague',
-	district: 'Old Town',
-	price: {
-		rent: 28000,
-		fees: {
-			water: 1000,
-			energy: 2500,
-			services: 560,
-		},
-		currency: 'czk',
+    pages: 350,
+    publisher: 'Bloomsbury',
+	year: 1997
+}
+const book2 = {
+    name: 'The Witcher',
+    author: {
+		firstname: 'Andrzej',
+		surname: 'Sapkowský'
 	},
-	ownership: 'personal',
-	condition: 'renovated',
-	status: 'free',
-	floor: 3,
+    pages: 400,
+    publisher: 'Leonardo',
+	year: 1993
 }
-
-document.body.innerHTML += "<p>Dispozice bytu: " + apartment.disposition +"</p>"
-document.body.innerHTML += "<p>Najem bytu: " + apartment.price.rent +"</p>"
-document.body.innerHTML += "<p>Výměra bytu: " + apartment.area.floorage + apartment.area.units +"</p>"
-
-//let realitka = document.querySelector('.realitka')
-//realitka.innerHTML = "Dispozice bytu: " + apartment.disposition + '<br>' + "Najem bytu: " + apartment.price.rent
-
-const city = apartment.city;
-const district = apartment.district;
-document.body.innerHTML += "<p>Mesto: "+ city +"</p>"
-document.body.innerHTML += "<p>Mestska cast: "+ district +"</p>"
-apartment.status = "taken"
-document.body.innerHTML += "<p>Stav inzeratu: "+ apartment.status +"</p>"
-
-// Knihovna
-const kniha = {
-    name: 'Moje kniha',
-    author: 'Autor',
-    pages: 12,
-    publisher: 'Albatros',
-
-}
-
-// Očkování
-//let vstupJmeno = prompt("Jmeno")
-//let vstupVek = Number(prompt('Vek'))
-
-const person = {
-    name: vstupJmeno,
-    age: vstupVek,
-}
-
-let nadpis = document.querySelector('h1')
-nadpis.innerText = person.name + ' ' + person.age
-
-// bonus ukol - seznam knízek - předpripravit si a vkladat tam nejaky dalsi knizky
-//git hub pages zkusit zprovoznit
+const library = [];
+library.push(book1);
+library.push(book2);
+console.log(library);
